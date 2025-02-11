c9ce9548ee8e49976736c3d287250be11b1addbc
Trevor <tmb7852@rit.edu>
2025-02-11 10:52:38 -0500

    Need a sanity check on why this is so broken

--- server/server.py
+++ server/server.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request
-from flask_restful import Api
+from flask_restful import Api, reqparse
 from flask_cors import CORS
 import pymongo
-import pprint
+import json
+import hashlib
 import utils
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 @app.route('/sample_connection', methods=['POST', 'GET'])
 def sample_connection():    
@@ -23,6 +25,45 @@ def sample_connection():
         return response
     else:
         return str(collection_access.find_one())
+    
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == "POST":
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str)
+        parser.add_argument('role', type=str)
+        parser.add_argument('password', type=str)
+        args = parser.parse_args()
+        name = args['name']
+        role = args['role']
+        password = args['password']
+        password = password.encode()
+        hashed_pass = hashlib.sha512(password).hexdigest()
+        utils.addUser(name, role, hashed_pass)
+    else:
+        return "Hello World"
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():    
+    if request.method == "POST":
+        parser = reqparse.RequestParser()
+        parser.add_argument('action', type=str)
+        parser.add_argument('name', type=str)
+        parser.add_argument('password', type=str)
+        parser.add_argument('session_key', type=str)
+        args = parser.parse_args()
+        action = args['action']
+        name = args['name']
+        if(action == 'login'):
+            password = args['password']
+            password = password.encode()
+            hashed_pass = hashlib.sha512(password).hexdigest()
+            return utils.login(name, hashed_pass)
+        else:
+            session_key = args['session_key']
+            return utils.logout(name, session_key)
+    else:
+        return "Hello World"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/utils.py
+++ server/utils.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import secrets
+import pymongo
+import datetime
 
 HEADSTRING = "mongodb://"
 base_url = "localhost:"
@@ -7,3 +10,30 @@ port = "27017"
 
 
 connection_string = HEADSTRING + base_url + port
+
+def make_connection(db):
+    mongo_client = pymongo.MongoClient(connection_string)
+    db_access = mongo_client[db]
+    return(db_access)
+
+def login(user, hashedPass):
+    db = make_connection("users")
+    result = db.authentication.find({"name": user, "password": hashedPass})
+    if(result == []):
+        return 0
+    name = result["name"]
+    session_key = secrets.token_urlsafe(32)
+    db.authentication.find_one_and_replace({"name": user, "password": hashedPass},{"session_token": session_key})
+    return [name, session_key]
+
+def logout(user, session_key):
+    db = make_connection("users")
+    result = db.authentication.find({"name": user, "session_token": session_key})
+    if(result == []):
+        return False
+    db.authentication.find_one_and_replace({"name": user, "session_token": session_key},{"session_token": None})
+    return True
+
+def addUser(name, role, password):
+    db = make_connection("users")
+    return db.authentication.insert_one({"name": name, "password": password, "role": role})
